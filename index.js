cf42f2437b9c5558c60da85472ada21f80e6aeb0
Alex <alexblackskja@gmail.com>
2022-11-26 19:25:26 +0300

    nodejs base task2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const yargs = require("yargs");
 const pkg = require("./package.json");
-const { addNote, getNotes } = require("./notes.controller");
+const { addNote, printNotes, deleteNote } = require("./notes.controller");
 
 yargs.version(pkg.version);
 
@@ -17,15 +17,29 @@ yargs.command({
     handler({ title }) {
         addNote(title);
     },
-});
+}); //  node index add --title=new     - добаление новой заметки с title new
+
+yargs.command({
+    command: "delete",
+    describe: "Remove note by id",
+    builder: {
+        id: {
+            type: "string",
+            descride: "Not id",
+            demandOption: true,
+        },
+    },
+    handler({ id }) {
+        deleteNote(id);
+    },
+}); //    node index delete --id=1669471743689 - удаление заметки с id 1669471743689
 
 yargs.command({
     command: "list",
     describe: "Print all notes",
-    handler() {
-        const notes = getNotes();
-        console.log(notes);
+    async handler() {
+        printNotes();
     },
-});
+}); //   node index list     -команда показ листа заметок
 
 yargs.parse();
--- notes.controller.js
+++ notes.controller.js
@@ -1,28 +1,48 @@
+// const { default: chalk } = require("chalk");
 const fs = require("fs/promises"); // подключаем file system
-const path = require("path");
+const path = require("path"); // подключаем пути
 
 const notesPath = path.join(__dirname, "db.json"); //делаем корректный путь к папке
 
 async function addNote(title) {
-    // const notes = require("./db.json");
-    // const notes = Buffer.from(buffer).toString("utf-8");
-    const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+    const notes = await getNotes();
+    const note = {
+        title,
+        id: Date.now().toString(),
+    };
 
-    console.log(typeof JSON.parse(notes));
-    // const note = {
-    //     title,
-    //     id: Date.now().toString(),
-    // };
-    // notes.push(note);
-    // await fs.writeFile(notesPath, JSON.stringify(notes)); // записываем новую запись в db.json
+    notes.push(note);
+    await fs.writeFile(notesPath, JSON.stringify(notes)); // записываем новую запись в db.json
+    console.log(`Note with title ${title} was added`);
+    // console.log(chalk.bgGreen("Note was added"));
 }
-addNote("jfkkf");
 
-function getNotes() {
-    return require(notesPath); // выдаем все записи из db.json
+async function getNotes() {
+    const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+    return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function deleteNote(id) {
+    const notes = await getNotes();
+
+    const newNotes = notes.filter((note) => note.id !== id);
+    await fs.writeFile(notesPath, JSON.stringify(newNotes)); // записываем новую запись в db.json
+    console.log(`Note with ${id} delete`);
+    // console.log(chalk.bgGreen("Note was added"));
+}
+
+async function printNotes() {
+    const notes = await getNotes();
+    // console.log(chalk.bgBlue("Here is the list of notes:"));
+    console.log("Here is the list of notes:");
+    notes.forEach((note) => {
+        // console.log(chalk.bgGray(note.title));
+        console.log(note.id, note.title);
+    });
 }
 
 module.exports = {
     addNote,
-    getNotes,
+    printNotes,
+    deleteNote,
 };
